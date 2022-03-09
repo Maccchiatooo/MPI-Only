76840f5a82d7d25115250ec8c5cacfbed0a906ed
Maccchiatooo <39581025+Maccchiatooo@users.noreply.github.com>
2022-03-09 16:05:46 +0100

    Update System.hpp

--- Cylinder flow/System.hpp
+++ Cylinder flow/System.hpp
@@ -8,32 +8,14 @@ class System
 {
 
 public:
-    System(int &nx, int &ny, int &nz) : sx(nx), sy(ny), sz(nz){};
-    void Initialize();
+    System();
     void Monitor();
 
-    //  domain size
     int sx, sy, sz;
-    // viscosity
-    double miu;
-    // Renolds number
-    double Re;
-    // input velocity
-    double u0;
-    // circle radius
-    int R;
-    // density
-    double rho0;
-    // speed of sound
-    double cs2;
-    double cs;
-    // Mach number
-    double Ma;
-    // total time
-    int Time;
-    // time interval
-    int inter;
-    // relaxation time connect to macro to micro
-    double tau;
+    int Time, inter;
+    double miu, u0, R, rho0, Ma, tau;
+
+    int Re;
+    double cs2, cs;
 };
 #endif
